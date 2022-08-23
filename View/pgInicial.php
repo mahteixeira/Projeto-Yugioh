4b5a3adc028851eaf812c75da0aedb09b0f5eae7
mahteixeira <teixeiramarina13@gmail.com>
2022-08-23 15:52:50 -0300

    ajustando modelagem do banco

--- View/pgInicial.php
+++ View/pgInicial.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
+    <style>
+        body{
+            background-image: linear-gradient(155deg, #e0b940, #5c2467 , #2b2467);
+        
+        }
+    </style>
 </head>
 <body>
     <h1> Carta criada com sucesso.  </h1>
